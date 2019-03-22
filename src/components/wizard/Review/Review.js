41e300aacd2fd04eca5e01442c544973261bcac1
James Sheehan <jmsdevx@gmail.com>
2019-03-22 15:12:57 -0500

    add review view

--- src/components/wizard/Review/Review.js
+++ src/components/wizard/Review/Review.js
@@ -1,5 +1,49 @@
 import React from "react";
+import { connect } from "react-redux";
+import { submitCharData } from "../../../ducks/wiz/wizAsync";
 
-const Review = () => <div>Review</div>;
+const Review = props => {
+  return (
+    <div className="review">
+      <h1 className="title">Review</h1>
+      <span>
+        <h4>Color: </h4>
+        <h5>{props.charData.color}</h5>
+      </span>
+      <span>
+        <h4>Race: </h4>
+        <h5>{props.charData.race}</h5>
+      </span>
+      <span>
+        <h4>Archetype: </h4>
+        <h5>{props.charData.archetype}</h5>
+      </span>
+      <span>
+        <h4>Fear: </h4>
+        <h5>{props.charData.fear}</h5>
+      </span>
+      <span>
+        <h4>Companion: </h4>
+        <h5>{props.charData.pet}</h5>
+      </span>
+      <span>
+        <h4>Connection: </h4>
+        <h5>{props.charData.family}</h5>
+      </span>
+    </div>
+  );
+};
 
-export default Review;
+const mapStateToProps = state => {
+  return {
+    charData: state.wizReducer.charData,
+    allChars: state.wizReducer.allChars,
+    pending: state.wizReducer.pending,
+    error: state.wizReducer.error
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { submit: submitCharData }
+)(Review);
